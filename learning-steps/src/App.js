4ead048605b1fd79f8f4dd075b545b13dd1682a5
aahmed1009 <3la2.a7med40@gmail.com>
2023-07-24 23:04:46 +0300

    handling evvents steps

--- learning-steps/src/App.js
+++ learning-steps/src/App.js
@@ -1,7 +1,31 @@
+const messages = ["learn react", "interviews", "invest your income"];
+
 export default function App() {
+  const step = 2;
   return (
-    <div>
-      <h2>lol</h2>
+    <div className="steps">
+      <div className="numbers">
+        <div className={step >= 1 ? "active" : ""}>1</div>
+        <div className={step >= 2 ? "active" : ""}>2</div>
+        <div className={step >= 3 ? "active" : ""}>3</div>
+      </div>
+      <div className="message">
+        Step {step}: {messages[step - 1]}
+      </div>
+      <div className="buttons">
+        <button
+          onClick={() => alert("back back back aaaa")}
+          style={{ backgroundColor: "red", color: "white" }}
+        >
+          Previous
+        </button>
+        <button
+          onClick={() => alert("next next next aaaa")}
+          style={{ backgroundColor: "blue", color: "white" }}
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
 }
